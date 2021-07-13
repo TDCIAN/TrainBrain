ac6753341c205d1b0c61e4285968b559eb54653a
JeongminKim <jeongminkim@APPLEui-MacBookPro.local>
2021-07-13 10:51:13 +0900

    뷰 추가

---
+++ TrainBrain/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  TrainBrain
+//
+//  Created by JeongminKim on 2021/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello, World!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrainBrain/TrainBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainBrainApp.swift
+//  TrainBrain
+//
+//  Created by JeongminKim on 2021/07/13.
+//
+
+import SwiftUI
+
+@main
+struct TrainBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
