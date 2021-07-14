07bc498b381405807615dd298dee24cd3b71bfb9
JeongminKim <jeongminkim@APPLEui-MacBookPro.local>
2021-07-14 16:56:24 +0900

    Made first app

--- TrainBrain/GameViewModel.swift
+++ TrainBrain/GameViewModel.swift
@@ -20,7 +20,7 @@ class GameViewModel: ObservableObject {
     
     private static func createGame() -> GameModel<String> {
         // numberOfPairsOfCards = 매칭 시킬 수 있는 카드 쌍의 개수
-        GameModel<String>(numberOfPairsOfCards: 16) { pairIndex in
+        GameModel<String>(numberOfPairsOfCards: 8) { pairIndex in
             emojis[pairIndex]
         }
     }
@@ -34,5 +34,13 @@ class GameViewModel: ObservableObject {
     // MARK: - Intent(s)
     func choose(_ card: Card) {
         model.choose(card)
+    }
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func restart() {
+        model = GameViewModel.createGame()
     }
 }
